12546d5dbe1f42c2b8123c2f6d62dd7e65368aaf
MADHUSOOTHANAN K <103616701+madhuthazhampattu@users.noreply.github.com>
2023-02-06 10:41:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import HomeComponent from "./modules/home";
+import Footer from "./footer";
+import "./app.css"
+
+const Container = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: column;
+  margin: 0 10px;
+  align-items: center;
+  height: 100vh;
+  width: 98%;
+  padding-top: 30px ;
+  font-family: Montserrat;
+`;
+
+const Header = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 22px;
+  font-size: 25px;
+  font-weight: bold;
+`;
+const App = () => {
+  return (
+    <Container>
+      <Header>Expense Tracker</Header>
+      <HomeComponent />
+      <Footer />
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/footer.js
@@ -0,0 +1,11 @@
+function Footer (){
+
+
+    return(
+        <footer className="footer1">
+        <a className="madhu" href="mailto:mathutnk@gmail.com">© copyright Madhuthazhampattu.com</a>
+        </footer>
+    )
+}
+
+export default Footer;
